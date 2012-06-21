7761ce5c00092515f358b546bd144386ec6d79d0
Jon Cram <jon@webignition.net>
2012-06-21 12:10:42 +0100

    Updated UserAgentDirectiveListTest, removed Record dependency

--- tests/useragentdirective/UserAgentDirectiveListTest.php
+++ tests/useragentdirective/UserAgentDirectiveListTest.php
@@ -4,45 +4,45 @@ require_once(__DIR__.'/../../lib/bootstrap.php');
 
 class UserAgentDirectiveListTest extends PHPUnit_Framework_TestCase {
     
-    public function testAdd() {         
-        $record = new \webignition\RobotsTxt\Record\Record();
+    public function testAdd() {
+        $userAgentDirectiveList = new \webignition\RobotsTxt\UserAgentDirective\UserAgentDirectiveList();
         
-        $record->userAgentDirectiveList()->add('googlebot');        
-        $this->assertEquals(array('googlebot'), $record->userAgentDirectiveList()->get());
+        $userAgentDirectiveList->add('googlebot');        
+        $this->assertEquals(array('googlebot'), $userAgentDirectiveList->get());
         
-        $record->userAgentDirectiveList()->add('slURp');
-        $this->assertEquals(array('googlebot', 'slurp'), $record->userAgentDirectiveList()->get());
+        $userAgentDirectiveList->add('slURp');
+        $this->assertEquals(array('googlebot', 'slurp'), $userAgentDirectiveList->get());
     }
     
     public function testRemove() {
-        $record = new \webignition\RobotsTxt\Record\Record();
+        $userAgentDirectiveList = new \webignition\RobotsTxt\UserAgentDirective\UserAgentDirectiveList();
         
-        $record->userAgentDirectiveList()->add('agent1');                
-        $record->userAgentDirectiveList()->add('agent2');
-        $record->userAgentDirectiveList()->add('agent3');        
-        $this->assertEquals(array('agent1', 'agent2', 'agent3'), $record->userAgentDirectiveList()->get());
+        $userAgentDirectiveList->add('agent1');                
+        $userAgentDirectiveList->add('agent2');
+        $userAgentDirectiveList->add('agent3');        
+        $this->assertEquals(array('agent1', 'agent2', 'agent3'), $userAgentDirectiveList->get());
         
-        $record->userAgentDirectiveList()->remove('aGEnt1');
-        $this->assertEquals(array('agent2', 'agent3'), $record->userAgentDirectiveList()->get());
+        $userAgentDirectiveList->remove('aGEnt1');
+        $this->assertEquals(array('agent2', 'agent3'), $userAgentDirectiveList->get());
         
-        $record->userAgentDirectiveList()->remove('agent2');
-        $this->assertEquals(array('agent3'), $record->userAgentDirectiveList()->get());
+        $userAgentDirectiveList->remove('agent2');
+        $this->assertEquals(array('agent3'), $userAgentDirectiveList->get());
         
-        $record->userAgentDirectiveList()->remove('agent3');
-        $this->assertEquals(array('*'), $record->userAgentDirectiveList()->get());        
+        $userAgentDirectiveList->remove('agent3');
+        $this->assertEquals(array('*'), $userAgentDirectiveList->get());        
     }
     
     public function testContains() {
-        $record = new \webignition\RobotsTxt\Record\Record();
+        $userAgentDirectiveList = new \webignition\RobotsTxt\UserAgentDirective\UserAgentDirectiveList();
         
-        $record->userAgentDirectiveList()->add('agent1');                
-        $record->userAgentDirectiveList()->add('agent2');
-        $record->userAgentDirectiveList()->add('AGent3');
+        $userAgentDirectiveList->add('agent1');                
+        $userAgentDirectiveList->add('agent2');
+        $userAgentDirectiveList->add('AGent3');
         
-        $this->assertTrue($record->userAgentDirectiveList()->contains('agent1'));
-        $this->assertTrue($record->userAgentDirectiveList()->contains('agent2'));
-        $this->assertTrue($record->userAgentDirectiveList()->contains('agent3'));
-        $this->assertFalse($record->userAgentDirectiveList()->contains('doesnotcontain'));
+        $this->assertTrue($userAgentDirectiveList->contains('agent1'));
+        $this->assertTrue($userAgentDirectiveList->contains('agent2'));
+        $this->assertTrue($userAgentDirectiveList->contains('agent3'));
+        $this->assertFalse($userAgentDirectiveList->contains('doesnotcontain'));
     }
     
 }
