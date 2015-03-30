81f40bf445c6130bfd08c3d227dc103d474ebcf4
Jon Cram <jon@webignition.net>
2015-03-30 08:32:26 +0100

    Refactor File::getDirectivesFor() test

--- tests/webignition/Tests/RobotsTxt/File/GetDirectivesFor/GetDirectivesForTest.php
+++ tests/webignition/Tests/RobotsTxt/File/GetDirectivesFor/GetDirectivesForTest.php
@@ -1,26 +1,9 @@
 <?php
 
-namespace webignition\Tests\RobotsTxt\File;
+namespace webignition\Tests\RobotsTxt\File\GetDirectivesFor;
 
-class GetDirectivesForTest extends FileTest {
+use webignition\Tests\RobotsTxt\File\FileTest;
 
-    public function testGetDirectivesFor() {
-        
-        $record1 = new \webignition\RobotsTxt\Record\Record();
-        $record1->userAgentDirectiveList()->add('*');
-        $record1->directiveList()->add('allow:/allowed-path-for-*');
-        $record1->directiveList()->add('disallow:/disallowed-path-for-*');
-        
-        $record2 = new \webignition\RobotsTxt\Record\Record();
-        $record2->userAgentDirectiveList()->add('googlebot');
-        $record2->directiveList()->add('allow:/allowed-path-for-googlebot');
-        $record2->directiveList()->add('disallow:/disallowed-path-for-googlebot');        
-        
-        $this->file->addRecord($record1);
-        $this->file->addRecord($record2);
-        
-        $this->assertEquals('allow:/allowed-path-for-googlebot'."\n".'disallow:/disallowed-path-for-googlebot', (string)$this->file->getDirectivesFor('googlebot'));
-        $this->assertEquals('allow:/allowed-path-for-*'."\n".'disallow:/disallowed-path-for-*', (string)$this->file->getDirectivesFor('*'));
-        $this->assertEquals('allow:/allowed-path-for-*'."\n".'disallow:/disallowed-path-for-*', (string)$this->file->getDirectivesFor('slurp'));
-    }
+abstract class GetDirectivesForTest extends FileTest {
+
 }
